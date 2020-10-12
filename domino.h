9b13763891a33f1f3598395b029921fe38a22420
Nathalia-git <57800589+Nathalia-git@users.noreply.github.com>
2020-10-12 01:44:34 +0000

    Create domino.h

---
+++ domino.h
@@ -0,0 +1,23 @@
+#ifndef DOMINO_H
+#define DOMINO_H
+
+#include "stdio.h"
+#include "stdlib.h"
+//#include "conio.h"  //para uso no windows
+#include "time.h"
+
+typedef struct Peca{
+	int E;
+	int D;
+	struct Peca *prox;
+}Peca;
+
+struct Peca *monte = NULL;
+struct Peca *jogador = NULL;
+struct Peca *tabuleiro = NULL;
+struct Peca *oponente = NULL;
+
+void principal_jogo();
+void inicio_jogo (Peca **percorre, Peca **corre);
+
+#endif
