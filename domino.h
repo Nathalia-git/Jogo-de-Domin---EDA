8e745552d24a5d3a5cbf515c0e759e48d375554c
Nathalia-git <57800589+Nathalia-git@users.noreply.github.com>
2020-10-13 17:59:01 +0000

    Update domino.h

--- domino.h
+++ domino.h
@@ -3,11 +3,17 @@
 
 #include "stdio.h"
 #include "stdlib.h"
-//#include "conio.h"  //para uso no windows
+//#include "conio.h"  //uso no windows
 #include "time.h"
-//cores para  jogo
+//cores para o jogo
 #define RS "\033[0m"
-#define FB "\033[34m"
+#define OL "\033[1m"
+#define FR "\033[31m" 
+#define FG "\033[32m" 
+#define FB "\033[34m" 
+#define FW "\033[37m" 
+#define BR "\033[41m" 
+#define BB "\033[44m" 
 
 typedef struct Peca{
 	int E;
@@ -21,8 +27,25 @@ struct Peca *tabuleiro = NULL;
 struct Peca *oponente = NULL;
 
 void principal_jogo();
-void inicio_jogo (Peca **percorre, Peca **corre);
+void inicio_jogo (Peca **percorre, Peca **corre);		
 void apresenta_jogo();	
-void apresenta(Peca *percorre, int verifica);	
+void apresenta(Peca *percorre, int verifica);		
+int jogador_jog(Peca *inicio, Peca *fim);		
+int oponente_jog(Peca **oponente, Peca *inicio, Peca *fim);		
+Peca* final_de_lista(Peca *comeco);	
+Peca* inicia(Peca *ptr);
+Peca* dispersar(Peca **inicio1, Peca **inicio2 , int qtd_pecas, int visualizar);		
+int contabiliza(Peca *percorre);		
+void conta_maior (Peca **percorre, Peca **corre);		
+Peca* arrevesar_peca(Peca *peca);
+Peca* seleciona();		
+void apresenta_posicao(Peca *percorre);	
+void para_baixo(Peca *percorre, int verifica);		
+int somatorio(Peca *percorre);		
+Peca* coloca_fim(Peca **lst, Peca **colocado);	
+int confere_peca(Peca *ptr, Peca *comeco, Peca *final); 
+void final_jogo();		
+void acabar(); 
+
 
 #endif
